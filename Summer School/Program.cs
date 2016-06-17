6ac3de89ce103b03d0a621e90c3eb064e4f04a32
Christopher Casasola <iamchiiyousee@yahoo.com>
2016-06-17 15:00:29 -0400

    added menu

---
+++ Summer School/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Summer_School
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Console.WriteLine("Please type the number of your selection:");
+                Console.WriteLine("1: Enroll a student");
+                Console.WriteLine("2: Unenroll a student");
+                Console.WriteLine("3: Print out the list of enrolled students");
+            }
+
+        }
+    }
+}
