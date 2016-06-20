8af78f809171947ef77af0c411d68bbc182dabad
Christopher Casasola <iamchiiyousee@yahoo.com>
2016-06-20 11:49:24 -0400

    Rewrote code from the bottom up to incorporate methods more easily

--- Summer School/Program.cs
+++ Summer School/Program.cs
@@ -10,34 +10,47 @@ namespace Summer_School
     {
         static void Main(string[] args)
         {
-            string[] Students = new string[15];
-         
-                Console.WriteLine("Please type the number of your selection:");
-                Console.WriteLine("1: Enroll a student");
-                Console.WriteLine("2: Unenroll a student");
-                Console.WriteLine("3: Print out the list of enrolled students");
-
-            double MainMenuSelection = Convert.ToInt32 (Console.ReadLine());
-            if (MainMenuSelection == 1)
+            MainMenu();
+            string menuChoice = Console.ReadLine();
+            int menuNumber = Convert.ToInt32(menuChoice);
+            if (menuNumber == 1)
             {
-
-                Console.WriteLine("Please type the name of the student you wish to enroll.");
-                string EnrolStudentSelection = Console.ReadLine();
-                for 
-                while ()
-                {
-
-                }
+                EnrollStudent();
             }
 
-            
-            else if (MainMenuSelection == 2)
+            else if (menuNumber == 2)
             {
-
-            
+                UnenrollStudent();
             }
+
+            else if (menuNumber == 3)
+            {
+                PrintMenu();
+            }
+
+            else if (menuNumber == 4)
+            {
                 
-            
+            }
+
+            else
+            {
+                Console.WriteLine("Please select a valid opton.");
+                MainMenu();
+            }
+
+ 
         }
+
+        static void MainMenu()
+        {
+            Console.WriteLine("Please type the number of your selection.");
+            Console.WriteLine("1: Enroll a student.");
+            Console.WriteLine("2: Unenroll a student.");
+            Console.WriteLine("3: View list of students.");
+            Console.WriteLine("Exit.");
+        }
+
+        
     }
 }
