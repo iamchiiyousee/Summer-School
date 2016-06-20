f98858aad7fe0b538f49eb98ad6cacc0d0d6f170
Christopher Casasola <iamchiiyousee@yahoo.com>
2016-06-19 23:08:43 -0400

    did some more stuff

--- Summer School/Program.cs
+++ Summer School/Program.cs
@@ -10,18 +10,24 @@ namespace Summer_School
     {
         static void Main(string[] args)
         {
-            string[]Students = [15];
+            string[] Students = new string[15];
          
                 Console.WriteLine("Please type the number of your selection:");
                 Console.WriteLine("1: Enroll a student");
                 Console.WriteLine("2: Unenroll a student");
                 Console.WriteLine("3: Print out the list of enrolled students");
 
-            double MainMenuSelection = Convert.ToInt32 (Console.ReadKey());
+            double MainMenuSelection = Convert.ToInt32 (Console.ReadLine());
             if (MainMenuSelection == 1)
             {
 
                 Console.WriteLine("Please type the name of the student you wish to enroll.");
+                string EnrolStudentSelection = Console.ReadLine();
+                for 
+                while ()
+                {
+
+                }
             }
 
             
